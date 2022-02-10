10d3816db17d5f4930f8f01a76abe820c0e0e03f
Csabák Balázs <csabak.balazs@fotex.net>
2022-02-10 16:52:39 +0100

    comments

--- src/contexts/classesContext.tsx
+++ src/contexts/classesContext.tsx
@@ -4,29 +4,27 @@ const initState = {};
 
 const ClassesContext = createContext(initState);
 
-const ClassesReducer = (state, action) => {
-	switch (action.type) {
-		case 'SET_TEST':
-			return { ...state, test: action.payload };
-
-		default:
-			return state;
-	}
+const ClassesReducer = () => {
+	// switch (action.type) {
+	// 	case 'SET_TEST':
+	// 		return { ...state, test: action.payload };
+	// 	default:
+	// 		return state;
+	// }
 };
 
-const ClassesProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(ClassesReducer, initState);
-
-	return (
-		<ClassesContext.Provider
-			value={{
-				state,
-				dispatch,
-			}}
-		>
-			{children}
-		</ClassesContext.Provider>
-	);
+const ClassesProvider = () => {
+	// const [state, dispatch] = useReducer(ClassesReducer, initState);
+	// return (
+	// 	<ClassesContext.Provider
+	// 		value={{
+	// 			state,
+	// 			dispatch,
+	// 		}}
+	// 	>
+	// 		{children}
+	// 	</ClassesContext.Provider>
+	// );
 };
 
 export { ClassesContext, ClassesProvider };
--- src/contexts/reservationContext.tsx
+++ src/contexts/reservationContext.tsx
@@ -1,41 +1,38 @@
-// import { useReducer, createContext } from 'react';
+import { useReducer, createContext } from 'react';
 
-// const initState = {};
+const initState = {};
 
-// const ReservationContext = createContext(initState);
+const ReservationContext = createContext(initState);
 
-// const ReservationReducer = (state, action) => {
-// 	switch (action.type) {
-// 		case 'SET_TEST':
-// 			return { ...state, test: action.payload };
+const ReservationReducer = () => {
+	// switch (action.type) {
+	// 	case 'SET_TEST':
+	// 		return { ...state, test: action.payload };
+	// 	default:
+	// 		return state;
+	// }
+};
 
-// 		default:
-// 			return state;
-// 	}
-// };
+const ReservationProvider = () => {
+	// const [reservationState, reservationDispatch] = useReducer(
+	// 	ReservationReducer,
+	// 	initState
+	// );
+	// const setShowtimeShowFilm = (data) => {
+	// 	reservationDispatch({
+	// 		type: 'SET_SHOWTIME_SHOW_FILM',
+	// 		payload: data,
+	// 	});
+	// };
+	// return (
+	// 	<ReservationContext.Provider
+	// 		value={{
+	// 			reservationState,
+	// 		}}
+	// 	>
+	// 		asd
+	// 	</ReservationContext.Provider>
+	// );
+};
 
-// const ReservationProvider = ({ children }) => {
-// 	const [reservationState, reservationDispatch] = useReducer(
-// 		ReservationReducer,
-// 		initState
-// 	);
-
-// 	const setShowtimeShowFilm = (data) => {
-// 		reservationDispatch({
-// 			type: 'SET_SHOWTIME_SHOW_FILM',
-// 			payload: data,
-// 		});
-// 	};
-
-// 	return (
-// 		<ReservationContext.Provider
-// 			value={{
-// 				reservationState,
-// 			}}
-// 		>
-// 			{children}
-// 		</ReservationContext.Provider>
-// 	);
-// };
-
-// export { ReservationContext, ReservationProvider };
+export { ReservationContext, ReservationProvider };
